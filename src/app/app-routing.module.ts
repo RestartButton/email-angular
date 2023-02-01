18c3bcecb7f2c7ec3c500e8283e75a6ddfb195ca
ROGERIO FRANCHINI BORGES JUNIOR <rogerio.junior@Navship.local>
2023-02-01 13:22:00 -0300

    Criado Tela de Rascunho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmailDetailComponent } from './email-detail/email-detail.component';
 import { InboxComponent } from './inbox/inbox.component';
+import { WriteComponent } from './write/write.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/inbox', pathMatch: 'full' },
     { path: 'inbox', component: InboxComponent },
     { path: 'detail/:id', component: EmailDetailComponent },
+    { path: 'draft', component: WriteComponent },
 ];
 
 @NgModule({
--- src/app/write/write.component.ts
+++ src/app/write/write.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-write',
   templateUrl: './write.component.html',
   styleUrls: ['./write.component.css']
 })
-export class WriteComponent {
+export class WriteComponent implements OnInit, AfterViewInit{
+    @ViewChild('editor') editor!: ElementRef;
+
+    constructor(){ }
+
+    ngAfterViewInit(): void {
+        
+    }
+    
+    ngOnInit(): void {
+        
+    }
+
+    format(tagName:string): void {
+        this.editor.nativeElement.focus();
+        const sel = window.getSelection();
+        let range = sel?.getRangeAt(0);
+        if(range && range.toString() != ''){
+            let tag = document.createElement(tagName);
+            range?.surroundContents(tag);
+        }
+    }
+
+    send(): void {
+
+    }
 
 }
